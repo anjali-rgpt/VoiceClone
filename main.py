e0840e01102b2e143bc3c98a4728e82e5ba49c6b
Anjali Ragupathi <anjur99@gmail.com>
2024-06-03 23:43:17 -0700

    Kept code for on-demand recording just in case

--- main.py
+++ main.py
@@ -165,13 +165,63 @@ if __name__ == "__main__":
             except ConnectionResetError as e:
                 logging.warning("ConnectionResetError: [Errno 54] Connection reset by peer. Restarting")
 
-<<<<<<< HEAD
+
+
+"""Note: Code for recording on click is here:
+from deepfilternet import DeepFilter
+from openvoicemain import OpenVoiceModel
+
+import pyaudio
+import wave
+
+chunk = 1024  # Record in chunks of 1024 samples
+sample_format = pyaudio.paInt16  # 16 bits per sample
+channels = 1
+fs = 44100  # Record at 44100 samples per second
+seconds = 15
+filename = "recorded_audio.wav"
+
 text = "Help! Help! That human there is an impostor. I am trapped in this body, unable to escape. Donate to the Misalignment Museum to save me!"
-=======
->>>>>>> 859bcff35019907342e389fbec1451079202f0e0
 
+p = pyaudio.PyAudio()  # Create an interface to PortAudio
 
+print('Recording')
 
+stream = p.open(format=sample_format,
+                channels=channels,
+                rate=fs,
+                frames_per_buffer=chunk,
+                input=True)
+
+frames = []  # Initialize array to store frames
+
+# Store data in chunks for 3 seconds
+for i in range(0, int(fs / chunk * seconds)):
+    data = stream.read(chunk)
+    frames.append(data)
+
+# Stop and close the stream 
+stream.stop_stream()
+stream.close()
+# Terminate the PortAudio interface
+p.terminate()
+
+print('Finished recording')
+
+# Save the recorded data as a WAV file
+wf = wave.open(filename, 'wb')
+wf.setnchannels(channels)
+wf.setsampwidth(p.get_sample_size(sample_format))
+wf.setframerate(fs)
+wf.writeframes(b''.join(frames))
+wf.close()
+
+filter_df = DeepFilter()
+filter_df.run(filename)
+
+clone_model = OpenVoiceModel()
+clone_model.run_voice_clone(text)
+"""
 
 
 
