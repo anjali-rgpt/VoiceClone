8b0e0fdf11f8ebb3c4b019c1295ab69d8a414334
Anjali Ragupathi <anjur99@gmail.com>
2024-06-04 23:26:04 -0700

    Added DFNet for SR and added cleanup code

--- deepfilternet.py
+++ deepfilternet.py
@@ -5,7 +5,7 @@ class DeepFilter():
     def __init__(self):
         pass
 
-    def run(self, input_audio_path):
-        result = subprocess.run(["deepfilter", input_audio_path, "--output-dir", "cleaned/"])
+    def run(self, input_audio_path, output_dir = "cleaned/"):
+        result = subprocess.run(["deepfilter", input_audio_path, "--output-dir", output_dir])
         # print(result.stdout)
 
